1e06f354bc56d8f7b201313b8fe23c272a75fc8b
anasswalha95 <anas.swalha@yahoo.com>
2021-01-24 00:48:39 +0200

    finish javascript functionalty for html files auto loader

---
+++ js/app.js
@@ -0,0 +1,69 @@
+'use strict'
+
+var d = document;
+
+
+// AutoLoad html files into index  page
+var headerIframe = d.getElementById('header-iframe')
+var asideIframe = d.getElementById('aside-iframe')
+var mainIframe = d.getElementById('main-iframe')
+var footerIframe = d.getElementById('footer-iframe')
+
+headerIframe.addEventListener('load', loadMainSection)
+asideIframe.addEventListener('load', loadMainSection)
+mainIframe.addEventListener('load', loadMainSection)
+footerIframe.addEventListener('load', loadMainSection)
+
+
+
+function loadMainSection(event) {
+    event.preventDefault();
+    this.before((this.contentDocument.body || this.contentDocument).children[0]);
+    this.remove()
+}
+
+window.addEventListener('load', function () {
+    var drumMachine = d.getElementById('drumMachine')
+    var sales = d.getElementById('sales')
+    var aboutUs = d.getElementById('aboutUs')
+
+
+
+    drumMachine.addEventListener('click', changeMainSection)
+    sales.addEventListener('click', changeMainSection)
+    aboutUs.addEventListener('click', changeMainSection)
+
+})
+
+function changeMainSection(event) {
+    event.preventDefault()
+
+    var main = d.getElementById('main')
+    main.remove()
+
+    var header = d.getElementById("header")
+    var footer = d.getElementById("footer")
+    var aside = d.getElementById("aside")
+
+
+    var iframe = d.createElement('iframe')
+
+    iframe.src = event.target.href
+
+    iframe.onload = function () {
+
+        this.before((this.contentDocument.body || this.contentDocument).children[0]);
+        this.remove()
+
+    };
+
+    document.body.appendChild(iframe).before(header)
+
+    document.body.removeChild(aside)
+    document.body.removeChild(footer)
+
+    document.body.appendChild(aside)
+    document.body.appendChild(footer)
+
+}
+
